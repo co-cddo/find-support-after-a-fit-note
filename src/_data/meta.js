0a257b4a75b50099ff0569e1ad74205a21c7c19d
Trevor Saint <trevorsaint@users.noreply.github.com>
2025-04-25 15:08:34 +0100

    Added opengraph image with meta configuration

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,18 @@
+const isProduction = process.env.ELEVENTY_ENV === "production";
+
 module.exports = {
   siteEnvironment: process.env.ELEVENTY_ENV || "development",
-  siteURL: "https://find-suppport-after-a-fit-note.service.gov.uk",
+
+  siteURL: isProduction
+    ? "https://co-cddo.github.io/find-support-after-a-fit-note/"
+    : "http://localhost:8080",
+
   serviceName: "Find support after a fit note",
   siteName: "GOV.UK",
   siteDescription: "Get help if you’re taking time off work due to a health condition",
-  siteEmail: "health-work-research@digital.cabinet-office.gov.uk"
+  siteEmail: "health-work-research@digital.cabinet-office.gov.uk",
+
+  siteOgImage: isProduction
+    ? "https://co-cddo.github.io/find-support-after-a-fit-note/assets/images/govuk-opengraph-image.png"
+    : "http://localhost:8080/assets/images/govuk-opengraph-image.png"
 }
