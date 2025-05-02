dfc8d3db7fb67ecba0e0eefe215ccf2042bde4d6
Trevor Saint <trevorsaint@users.noreply.github.com>
2025-05-02 16:56:37 +0100

    Various tweaks for path issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,27 +1,21 @@
-// const isProduction = process.env.ELEVENTY_ENV === "production";
-// const pathPrefix = isProduction ? "/find-support-after-a-fit-note/" : "/";
-
 const pathPrefix = process.env.ELEVENTY_PATH_PREFIX || "/";
 
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = async function (eleventyConfig) {
-
-
-  // Make pathPrefix globally available in my templates
+  
+  // Make pathPrefix globally available in templates
   eleventyConfig.addGlobalData("pathPrefix", pathPrefix);
 
-
   // Fitnote collection
-  eleventyConfig.addCollection("fitnote", function(collectionApi) {
+  eleventyConfig.addCollection("fitnote", function (collectionApi) {
     return collectionApi.getFilteredByTag("fitnote").sort((a, b) => {
       return a.data.eleventyNavigation.order - b.data.eleventyNavigation.order;
     });
   });
 
-
-  // Get previous and next
-  eleventyConfig.addNunjucksFilter("stripPreviewPrefix", function(url) {
+  // Filters for previous and next navigation, cleaning preview prefix
+  eleventyConfig.addNunjucksFilter("stripPreviewPrefix", function (url) {
     return url.replace(/^\/preview\/pr-\d+\//, "/");
   });
 
@@ -31,7 +25,7 @@ module.exports = async function (eleventyConfig) {
     if (index > 0) return collection[index - 1];
     return null;
   });
-  
+
   eleventyConfig.addNunjucksFilter("getNextCollectionItem", function (collection, currentUrl) {
     const cleanUrl = currentUrl.replace(/^\/preview\/pr-\d+\//, "/");
     const index = collection.findIndex(item => item.url === cleanUrl);
@@ -39,60 +33,44 @@ module.exports = async function (eleventyConfig) {
     return null;
   });
 
-
+  // Add heading IDs
   const { IdAttributePlugin } = await import("@11ty/eleventy");
-
-
-  // Add IDs for headings
   eleventyConfig.addPlugin(IdAttributePlugin, {
-		selector: "h2, h3, h4, h5, h6",
-		decodeEntities: true,
-		checkDuplicates: true,
-		slugify: eleventyConfig.getFilter("slugify")
-	});
+    selector: "h2, h3, h4, h5, h6",
+    decodeEntities: true,
+    checkDuplicates: true,
+    slugify: eleventyConfig.getFilter("slugify")
+  });
 
-
-  // Navigation
+  // Navigation plugin
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-
-  // Copy GOVUK assets, manifest and cookie assets
-  eleventyConfig.addPassthroughCopy({ 
+  // Passthrough assets
+  eleventyConfig.addPassthroughCopy({
     "node_modules/govuk-frontend/dist/govuk/assets/images/": "assets/images",
     "node_modules/govuk-frontend/dist/govuk/assets/fonts/": "assets/fonts",
     "src/assets/manifest.json": "assets/manifest.json",
     "src/assets/scripts/cookie-manager.js": "assets/scripts/cookie-manager.js"
   });
 
-
-  // Fix path if inside a sub folder
-  // eleventyConfig.addGlobalData("pathPrefix", process.env.ELEVENTY_ENV === "production" ? "/find-support-after-a-fit-note" : "");
-
-
-  // Link path in markdown
-  eleventyConfig.addShortcode("link", function(path, label) {
-    const pathPrefix = this.ctx.pathPrefix || "";
-    const fullPath = `${pathPrefix}${path}`.replace(/\/{2,}/g, "/");
-    return `<a href="${fullPath}">${label}</a>`;
+  // Shortcode for links with pathPrefix
+  eleventyConfig.addShortcode("link", function (path, label) {
+    return `<a href="${`${this.ctx.pathPrefix}${path}`.replace(/\/{2,}/g, "/")}">${label}</a>`;
   });
 
-
-  // Add a filter to join paths with the pathPrefix
-  eleventyConfig.addFilter("absoluteUrl", function(path) {
-    return `${pathPrefix || ""}${path}`.replace(/\/\/+/g, "/");
+  // Filter to join paths with pathPrefix
+  eleventyConfig.addFilter("absoluteUrl", function (path) {
+    return `${pathPrefix}${path}`.replace(/\/{2,}/g, "/");
   });
 
-
-  // Filters
+  // Custom filters
   require("./config/filters/merge-filter.js")(eleventyConfig);
   require("./config/filters/merge-objects.js")(eleventyConfig);
-
 
   // Plugins
   eleventyConfig.addPlugin(require("./config/plugins/scss-config.js"));
   eleventyConfig.addPlugin(require("./config/plugins/html-config.js"));
   eleventyConfig.addPlugin(require("./config/plugins/js-config.js"));
-
 
   return {
     dir: {
@@ -107,7 +85,5 @@ module.exports = async function (eleventyConfig) {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk"
-  }
-
-
+  };
 };
